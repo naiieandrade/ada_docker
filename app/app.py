1b3bf931b6fcf6047fd28827b0ae42cec0d9b2cb
Naiara Andrade <andradenaiara@hotmail.com>
2024-02-28 18:30:23 -0300

    Update project with connection between application and database

--- app/app.py
+++ app/app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://my_user:my_password@db/my_database'
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user_ada:password_ada@db/db_ada'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello():
-	#return 'Hello, Flask in Docker with Database NAIARAs!'
+class Dados(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_input = db.Column(db.String(255), nullable=False)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        novo_dado = Dados(user_input=user_input)
+        db.session.add(novo_dado)
+        db.session.commit()
+        return f'Você enviou: {user_input} e os dados foram adicionados ao banco!'
     return render_template('index.html')
 
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    create_tables()
+    app.run(debug=True, host='0.0.0.0')
